543ae8730b9fc9cafd22c4230d9eab6e3fd8e935
subiksha1208 <ssubiksha87@gmail.com>
2025-10-29 17:48:51 +0530

    Delete supermarket-backend directory

--- supermarket-backend/src/main/java/com/supermarket/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.supermarket.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.supermarket.model.Bill;
-
-public interface BillRepository extends MongoRepository<Bill, String> {
-}
--- supermarket-backend/src/main/java/com/supermarket/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.supermarket.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.supermarket.model.Customer;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-    Customer findByEmail(String email);
-}
